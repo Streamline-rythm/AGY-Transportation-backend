5e401c3a6667dbd80f48ee23ce4be183a59afb1f
smooth rythm <rythmsmooth@gmail.com>
2025-07-05 03:34:40 +0700

    for google cloud

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const driversRouter = require('./routes/index'); // or wherever your router is
-
+app.use(cors());
 app.use(express.json());
 app.use('/api', driversRouter);  // e.g., endpoint will be /api/getAllDriversData
-
 module.exports = app;
